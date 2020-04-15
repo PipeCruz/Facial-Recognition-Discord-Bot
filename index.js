99129e0c5000559a1b41e9ee52580c428a3a6ee2
PipeCruz <43627567+PipeCruz@users.noreply.github.com>
2020-04-15 03:03:13 -0400

    Face detection complete, time for proper usage of files and compression

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const fs = require('fs');
 const frontFaceClassifier = new cv.CascadeClassifier(cv.HAAR_FRONTALFACE_DEFAULT);
 // const eyeClassifier = new cv.CascadeClassifier(cv.HAAR_EYE);
 const eyeClassifier = new cv.CascadeClassifier(cv.HAAR_EYE);
-const userRe = /<@!?\d{18}>/;
-const regID = /\d{18}/;
+const mentionedRegex = /<@!?\d{18}>/;
+const userIDRegex = /\d{18}/;
 
 client.once('ready', () => {
     console.log('Logged in');
@@ -19,66 +19,56 @@ client.once('ready', () => {
     });
 });
 
-function faceDet(imageIn, channel) {
-    console.log(imageIn);
+// fix file type
+function newFaceDet(imageIn, channel) {
     https.request(imageIn, (response) => {
+        console.log(imageIn);
         const data = new Stream();
+        let totalbytes = 0;
         response.on('data', (d) => {
             console.log(`Received ${d.length} bytes of data.`);
+            totalbytes += d.length;
             data.push(d);
         });
-        response.on('end', () => {
+        // once or on?
+        response.once('end', ()=> {
+            console.log(`total bytes: ${totalbytes}`);
             fs.writeFileSync('temp/image.png', data.read());
-            if(cv.imread('temp/image.png') === null) {
-                return;
-            }
             const mat = cv.imread('temp/image.png');
             const matGray = mat.bgrToGray();
-            frontFaceClassifier.detectMultiScaleAsync(matGray, (err, res)=> {
-                // error catch here
-                res.objects.forEach(t => {
-                    console.log(`x = ${t.x}`);
-                    console.log(`y = ${t.y}`);
-                    console.log(`width = ${t.width}`);
-                    console.log(`height = ${t.height}`);
-                    console.log('--------------');
-                    const point1 = new cv.Point2(t.x, t.y);
-                    const point2 = new cv.Point2(t.x + t.width, t.y + t.height);
-                    mat.drawRectangle(point1, point2, new cv.Vec(255, 0, 0));
+            const faceObjs = frontFaceClassifier.detectMultiScale(matGray).objects;
+            console.log(`Number of faces detected is ${faceObjs.length}`);
+            faceObjs.forEach(rect =>{
+                // draw face rectangles
+                const point1 = new cv.Point2(rect.x, rect.y);
+                const point2 = new cv.Point2(rect.x + rect.width, rect.y + rect.height);
+                mat.drawRectangle(point1, point2, new cv.Vec3(0, 0, 255));
+                // eye detection
+                const sectionForEyes = matGray.getRegion(rect);
+                const eyesDetected = eyeClassifier.detectMultiScale(sectionForEyes).objects;
+                console.log(`eyeObjects has ${eyesDetected.length}`);
+                let eyeCnt = 0;
+                eyesDetected.forEach((eye) => {
+                    if(eyeCnt++ < 2) {
+                        const ePoint1 = new cv.Point2(eye.x, eye.y);
+                        const ePoint2 = new cv.Point2(eye.x + eye.width, eye.y + eye.height);
+                        mat.getRegion(rect).drawRectangle(ePoint1, ePoint2, new cv.Vec3(255, 0, 0));
+                    }
                 });
-                cv.imwrite('temp/img.png', mat);
             });
-            // temp
-            // Write eyes
-            eyeClassifier.detectMultiScaleAsync(matGray, (err, res)=> {
-                // let inte = 0;
-                res.objects.forEach(t => {
-                    // if (inte++ < (faces * 2)) {
-                    // console.log(`EYE ${inte}`);
-                    console.log(`x = ${t.x}`);
-                    console.log(`y = ${t.y}`);
-                    console.log(`width = ${t.width}`);
-                    console.log(`height = ${t.height}`);
-                    console.log('--------------');
-                    const point1 = new cv.Point2(t.x, t.y);
-                    const point2 = new cv.Point2(t.x + t.width, t.y + t.height);
-                    mat.drawRectangle(point1, point2, new cv.Vec(0, 0, 255));
-                    // }
-                });
-                cv.imwrite('temp/img.png', mat);
-            });
+            cv.imwrite('temp/img.png', mat);
             channel.send(new Discord.MessageAttachment('temp/img.png'));
         });
     }).end();
 }
 
-client.on('message', async m => {
+client.on('message', m => {
     if(!m.content.startsWith(prefix) || m.author.bot)return;
     const args = m.content.split(' ');
-    const passesIDTest = userRe.test(args[1]) || regID.test(args[1]);
+    const passesIDTest = mentionedRegex.test(args[1]) || userIDRegex.test(args[1]);
     if(args[0].toLowerCase() === `${prefix}pfp`) {
         if(passesIDTest) {
-            const id = args[1].match(regID)[0];
+            const id = args[1].match(userIDRegex)[0];
             m.guild.members.fetch(id).then(gMember=>{
                 m.channel.send({
                     files: [gMember.user.displayAvatarURL()]
@@ -90,15 +80,16 @@ client.on('message', async m => {
             }).then().catch();
         }
     }else if(args[0].toLowerCase() === `${prefix}facedet`) {
-        if(passesIDTest) {
-            const id = args[1].match(regID)[0];
+        // fixme since it looks for 18 characters in url
+        if(args[1].substr(0, 5) === 'https') {
+            newFaceDet(args[1], m.channel);
+        }else if(passesIDTest) {
+            const id = args[1].match(userIDRegex)[0];
             m.guild.members.fetch(id).then(gMember=>{
-                faceDet(gMember.user.displayAvatarURL(), m.channel);
+                newFaceDet(gMember.user.displayAvatarURL(), m.channel);
             });
         }else if(args.length === 1) {
-            faceDet(m.author.displayAvatarURL(), m.channel);
-        }else if(args[1].substr(0, 5) === 'https') {
-            faceDet(args[1], m.channel);
+            newFaceDet(m.author.displayAvatarURL(), m.channel);
         }
     }else if(args[0] === `${prefix}ping`) {
         m.channel.send('Pong!.. I dont want to find the delay so this is what you\'re going to deal with');
